d4f4b873f406c784e61f49649a5528a907ea7359
Asante-Adarkwa-Usman <asante.au96@gmail.com>
2020-06-16 02:04:12 +0000

    mobile app for FriendsBuilt team

---
+++ FriendsbuiltBot/index.js
@@ -0,0 +1,6 @@
+
+import {AppRegistry} from 'react-native';
+import Navigation from './Navigation';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Navigation);
